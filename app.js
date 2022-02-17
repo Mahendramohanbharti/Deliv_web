c5164bf23d4e28510ec3f36e6ad922eab649f231
Mahendra Mohan Bharti <bhartisiddharth00000@gmail.com>
2022-02-17 20:12:44 +0530

    REview 1 Commit

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express= require('express');
 var path =require('path');
 const mongoose = require('mongoose');
 var config=require('./config/database');
+var bodyParser=require('body-parser');
+var session=require('express-session');
+var expressValidator = require('express-validator');
 
 //connect to db
 mongoose.connect(config.database);
@@ -21,15 +24,83 @@ var app=express();
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine','ejs');
 
+
+
 //set public folder
 
 app.use(express.static(path.join(__dirname,'public')));
 
-app.get('/',function(req,res){
-    res.render('index',{
-        title:'Home'
-    })
+// set global error variable
+
+app.locals.errors=null;
+
+// bodyparser middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+//express session middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true,
+    // cookie: { secure: true }
+  }))
+
+  // express validator middleware
+  app.use(expressValidator({
+    errorFormatter: function (param, msg, value) {
+        var namespace = param.split('.')
+                , root = namespace.shift()
+                , formParam = root;
+
+        while (namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param: formParam,
+            msg: msg,
+            value: value
+        };
+    }
+    // customValidators: {
+    //     isImage: function (value, filename) {
+    //         var extension = (path.extname(filename)).toLowerCase();
+    //         switch (extension) {
+    //             case '.jpg':
+    //                 return '.jpg';
+    //             case '.jpeg':
+    //                 return '.jpeg';
+    //             case '.png':
+    //                 return '.png';
+    //             case '':
+    //                 return '.jpg';
+    //             default:
+    //                 return false;
+    //         }
+    //     }
+    // }
+}));
+
+//express messages middleware
+
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
 });
+
+//Set routes
+
+var pages=require('./routes/pages.js');
+var adminPages=require('./routes/admin_pages.js')
+var adminCategories=require('./routes/admin_categories.js')
+
+app.use('/admin/pages',adminPages);
+app.use('/admin/categories',adminCategories);
+app.use('/',pages);
 
 //Start server
 
